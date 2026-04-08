4577a256576daee59242e072cd510c98511c68f7
soobinmun <msb1220@snu.ac.kr>
2026-04-08 11:31:18 +0900

    Complete main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,23 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+   if (cnt==M){
+      for (int i=0; i<M; i++){
+         cout << vec[i] << " ";
+      }
+      cout << endl;
+      return 0;
+   }
+   int start=1;
+   if (!vec.empty()){
+      start=vec.back+1;
+   }
 
-
-
-
-
-
+   for (int i=start; i <= N; i++){
+      vec.push_back(i);
+      func(cnt + 1);
+      vec.pop_back();
+   }
 
 }
 
